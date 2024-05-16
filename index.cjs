23e540b20f5c51b38fec9cfb4b8b1acb0c46dac1
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2024-05-15 21:03:55 -0600

    fix: main import (#1)
    * fix: set outputs and fix imports
    
    * chore: change index extension
    
    * chore: save

---
+++ index.cjs
@@ -0,0 +1,29 @@
+const core = require("@actions/core");
+const lo = require("lodash");
+
+const Action = require("./src/action.js");
+
+const main = async () => {
+  let paths = core.getInput("paths");
+  let separator = core.getInput("separator");
+
+  if (lo.isEmpty(separator)) {
+    separator = `\n`;
+  }
+
+  const action = new Action(paths, separator);
+
+  try {
+    const result = action.run();
+
+    core.setOutput("total", result.total);
+    core.setOutput("folders", result.folders);
+    core.setOutput("folders_no_base_path", result.foldersNoBasePath);
+    core.setOutput("folders_by_path", result.foldersByPath);
+  } catch (e) {
+    core.error(`[ERROR] Unexpected failure listing subfolders: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+main();
